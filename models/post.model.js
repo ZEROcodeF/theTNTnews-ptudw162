94cd0f28eedf8d5f51445d4d40a0254dfaa709c8
ZEROcodeF <zerocodef@gmail.com>
2019-06-12 16:26:23 +0700

    Dynamic Image View Fix. Minor layout fix

--- models/post.model.js
+++ models/post.model.js
@@ -8,7 +8,7 @@ module.exports = {
   //BEGIN For Homepage:
   //Featured Posts:  FOUR posts which have most viewed last week (exact 7 days from 'now')
   featuredPosts: () =>{
-    return db.load(`select post_id, post_type, post_category, post_title, post_time, post_thumbnail, category_name, category_class from (select * from post where post_time between date_sub(now(), interval 7 day) and now()) p join category on p.post_category=category_id where post_status ='publish' and now() >=  post_time order by post_viewcount desc limit 4`);
+    return db.load(`select post_id, post_type, post_category, post_title, post_time, post_bigthumbnail, category_name, category_class from (select * from post where post_time between date_sub(now(), interval 7 day) and now()) p join category on p.post_category=category_id where post_status ='publish' and now() >=  post_time order by post_viewcount desc limit 4`);
   },
 
   //latestPosts: TEN posts which have latest publish time
